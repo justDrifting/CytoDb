e8256d4563f49c315221b3b0852d81cf18f2fcf4
Bobby <Bobby@MacBook-Pro-2.local>
2014-03-10 08:15:21 -0500

    Initial Commit

---
+++ CytoDb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CytoDb
+//
+//  Created by Bobby on 3/10/14.
+//  Copyright (c) 2014 Rifter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDBAppDelegate class]));
+    }
+}
